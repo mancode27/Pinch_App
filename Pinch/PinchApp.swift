cd9fe83ee396a8222745ed282bf571bb25082c6a
ManojKumar Bellatti <manojkumarb.cs20@rvce.edu.in>
2022-08-23 21:33:47 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Manojkumar Bellatti on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
